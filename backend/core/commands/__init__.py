0f9489c98bd9c8b4dc0e924f5c7bf3e983b9c6a1
Sam <65622420+Sammyco921@users.noreply.github.com>
2026-05-30 20:01:23 -0500

    Delete backend directory

--- backend/core/commands/__init__.py
+++
@@ -1,6 +0,0 @@
-from core.commands.command_router import CommandIntent, CommandRouter
-
-__all__ = [
-    "CommandIntent",
-    "CommandRouter",
-]
--- backend/core/handoff/__init__.py
+++
@@ -1,6 +0,0 @@
-from core.handoff.package import HandoffBuilder, HandoffPackage
-
-__all__ = [
-    "HandoffBuilder",
-    "HandoffPackage",
-]
